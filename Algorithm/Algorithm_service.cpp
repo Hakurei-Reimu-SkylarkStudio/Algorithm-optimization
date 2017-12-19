a8e68aa045dd38cb89d7f08e46472da502d0552a
涂山月 <LLQsky@live.cn>
2017-12-19 15:36:16 +0800

    201712191536

--- Algorithm/Algorithm_service.cpp
+++ Algorithm/Algorithm_service.cpp
@@ -1,7 +1,5 @@
 //Algorithm_service.cpp
 
-
-
 //文件读写：http://blog.csdn.net/kingstar158/article/details/6859379/
 
 #include<iostream>
@@ -22,26 +20,28 @@ typedef int status;
 
 int E[MAX_SIZE][MAX_SIZE];	//构建边关系（E）
 
-
-class SP //服务实例
+struct SP
 {
-public:
-	status initialization();	//初始化
-	int l = { 0 };	//服务个数
-	int s[MAX_SIZE] = { 0 };	//服务名称
-	int c[MAX_SIZE] = { 0 };	//服务费用
-	int t[MAX_SIZE] = { 0 };	//服务时间
+	int t[1];
+	int s[1];
+	int c[1];
 };
-
-status SP::initialization()
+struct V
 {
-	return SUCCESS;
-}
+	SP sp[1];
+};
+struct G
+{
+	V v[1];
+};
 
 status globalLnitialization();
 
 int main()
 {
+	G g;
+	
+
 	SP sp[300];
 	string strTemp;	//临时储存
 	int intTemp;	//临时储存
@@ -58,18 +58,3 @@ status initialization()
 {
 	return SUCCESS;
 }
-//
-//int main() {
-//	string buffer;
-//	ifstream in("MatIn.txt");
-//	if (!in.is_open())
-//	{
-//		cout << "Error opening file"; exit(1);
-//	}
-//	while (!in.eof())
-//	{
-//		in.getline(buffer, 100);
-//		cout << buffer << endl;
-//	}
-//	return 0;
-//}
